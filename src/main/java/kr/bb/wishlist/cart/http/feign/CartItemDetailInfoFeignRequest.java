7a79c73988d369e88f2fd9c06bfdcce005336e92
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-10 08:37:50 +0900

    fix: fix the missed fegin target url

--- src/main/java/kr/bb/wishlist/cart/http/feign/CartItemDetailInfoFeignRequest.java
+++ src/main/java/kr/bb/wishlist/cart/http/feign/CartItemDetailInfoFeignRequest.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "get-cart-items-detail")
+@FeignClient(name = "get-cart-items-detail",url = "${service.product.domain}")
 public interface CartItemDetailInfoFeignRequest {
   @GetMapping("/products/carts")
   public ResponseEntity<GetUserCartItemsResponse> getCartItemDetails(@Valid @RequestBody CartItemProductIdDto productIdList);
--- src/main/java/kr/bb/wishlist/cart/repository/CartJpaRepository.java
+++ src/main/java/kr/bb/wishlist/cart/repository/CartJpaRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 import kr.bb.wishlist.cart.entity.CartCompositekey;
 import kr.bb.wishlist.cart.entity.CartEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 
 public interface CartJpaRepository extends JpaRepository<CartEntity, CartCompositekey> {
     List<CartEntity> findAllByCartCompositekey_UserId(Long userId);
