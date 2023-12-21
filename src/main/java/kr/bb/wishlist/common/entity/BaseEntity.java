eec857ad3dfc65931a139dc7c6a79a70fd4f4285
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-21 15:32:59 +0900

    fix: fix the dev to prod

--- src/main/java/kr/bb/wishlist/common/entity/BaseEntity.java
+++ src/main/java/kr/bb/wishlist/common/entity/BaseEntity.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -16,13 +17,16 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class BaseEntity {
 
   @CreatedDate
-  @Column(name = "created_at")
+  @ColumnDefault("now()")
+  @Column(name = "created_at", columnDefinition = "TIMESTAMP", nullable = false)
   private LocalDateTime createdAt;
 
   @LastModifiedDate
-  @Column(name = "updated_at")
+  @ColumnDefault("now()")
+  @Column(name = "updated_at", columnDefinition = "TIMESTAMP", nullable = false)
   private LocalDateTime updatedAt;
 
   @Column(name = "is_deleted")
-  private Boolean isDeleted;
+  private Boolean isDeleted = false;
+
 }
